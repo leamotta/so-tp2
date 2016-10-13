4e7e27fc581273a6b7a2fd2f013ca63fb15972be
Diego Sueiro <diegos@semexpert.com.ar>
2016-10-13 13:39:12 -0300

    Avance con RwLock

--- locks/RWLock.cpp
+++ locks/RWLock.cpp
@@ -1,17 +1,55 @@
 #include "RWLock.h"
 
 RWLock :: RWLock() {
-
+    reading = 0;
+    writing = 0;
+    writers = 0;
 }
 
 void RWLock :: rlock() {
+    while(1) {
+        pthread_cond_wait(&turn, &m);
+        if (writing <= 0 && writers <=0) {
+            reading++;
+            pthread_cond_signal(&turn);
+            break;
+        }
+        pthread_cond_signal(&turn);
+        pthread_cond_wait(&read_cond, &m_r);
+    }
 }
 
 void RWLock :: wlock() {
+    while(1) {
+        pthread_cond_wait(&turn, &m);
+        writers++;
+        if (reading <= 0 && writing <=0) {
+            writing++;
+            writers--;
+            pthread_cond_signal(&turn);
+            break;
+        }
+        pthread_cond_signal(&turn);
+        pthread_cond_wait(&write_cond, &m_w);
+    }
 }
 
 void RWLock :: runlock() {
+    pthread_cond_wait(&turn, &m);
+    reading--;
+    pthread_cond_signal(&turn);
+    pthread_cond_signal(&write_cond);
 }
 
 void RWLock :: wunlock() {
+    pthread_cond_wait(&turn, &m);
+    writing--;
+    if (writers > 0) {
+        // Libero otro writer
+        pthread_cond_signal(&write_cond);
+    } else {
+        // Libero reading
+        pthread_cond_signal(&read_cond);
+    }
+    pthread_cond_signal(&turn);
 }
--- locks/RWLock.h
+++ locks/RWLock.h
@@ -1,6 +1,7 @@
 #ifndef RWLock_h
 #define RWLock_h
 #include <iostream>
+#include <pthread.h>
 
 class RWLock {
     public:
@@ -13,6 +14,12 @@ class RWLock {
     private:
         pthread_mutex_t m;
         pthread_cond_t  turn;   /* Event: someone else's turn */
+
+        pthread_mutex_t m_r;
+        pthread_cond_t  read_cond;   
+
+        pthread_mutex_t m_w;
+        pthread_cond_t  write_cond;   
 
         int reading;
         int writing;
